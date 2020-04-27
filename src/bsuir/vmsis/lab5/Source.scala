6e84dd7e5ecb6c146a697451f2ad0d07ca64ec13
unknown <andrei.karymau@gmail.com>
2020-04-28 00:39:02 +0300

    lab 5 v 12

---
+++ src/bsuir/vmsis/lab5/Source.scala
@@ -0,0 +1,15 @@
+package bsuir.vmsis.lab5
+
+import bsuir.vmsis.lab5.wprocessor.WordProcessor
+
+object Source {
+  def main(args: Array[String]){
+    println("Enter a string to reverse")
+    val input = scala.io.StdIn.readLine()
+    println("Tail recursion; reversed string:")
+    println(WordProcessor.reverseStringWithTailRecursion(input))
+    println("Recursion without tail call; reversed string:")
+    println(WordProcessor.reverseString(input))
+    
+  }
+}
---
+++ src/bsuir/vmsis/lab5/wprocessor/WordProcessor.scala
@@ -0,0 +1,31 @@
+package bsuir.vmsis.lab5.wprocessor
+
+import scala.annotation.tailrec
+
+object WordProcessor {
+    def reverseString(string: String): String = {
+      var mutableString= new StringBuilder(string)
+      def recursion(index: Int): StringBuilder ={
+        if(mutableString.size>index)
+          recursion(index+1)+=mutableString.charAt(index)
+        else
+          new StringBuilder(mutableString.size)
+      }
+      recursion(0).toString()
+    }
+    
+    def reverseStringWithTailRecursion(string: String): String = {
+      var mutableString= new StringBuilder(string)
+      @tailrec
+      def tailRecursion(beginIndex: Int, endIndex: Int){
+        if(endIndex-beginIndex>1){
+          val charBuffer=mutableString.charAt(beginIndex)
+          mutableString.setCharAt(beginIndex, mutableString.charAt(endIndex))
+          mutableString.setCharAt(endIndex, charBuffer)
+          tailRecursion(beginIndex+1, endIndex-1)
+        }
+      }
+      tailRecursion(0, mutableString.size-1)
+      mutableString.toString()
+    }
+}
